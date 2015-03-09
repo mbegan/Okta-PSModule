8cbf51859f40eaa96bc7729abe85e9ad25eedce2
Matt Egan <mbegan@gmail.com>
2015-03-09 11:45:07 -0600

    watch Matt flounder with git

--- Okta_org.ps1
+++ Okta_org.ps1
@@ -3,13 +3,13 @@
 
 [Hashtable]$oktaOrgs = @{
                         prod = [Hashtable]@{
-                                            baseUrl  = [String]"https://YourDomain.okta.com"
+                                            baseUrl  = [String]"https://varian.okta.com"
                                             secToken = [String]"YourAPIToken"
                                             enablePagination = [boolean]$true
                                             pageSize = [int]500
                                            }
                         prev = [HashTable]@{
-                                            baseUrl  = [String]"https://YourDomain.oktapreview.com"
+                                            baseUrl  = [String]"https://varian.oktapreview.com"
                                             secToken = [String]"YourAPIToken"
                                             enablePagination = [boolean]$true
                                             pageSize = [int]500
