77675e0b518b0f19b6dfae4c7733c786d79dc387
AWBbox <AWBbox@users.noreply.github.com>
2019-03-26 12:24:56 +0000

    roleId validation correction
    The validation minimum/maximum length for the roleId value has been corrected, I discovered today that it can be lower than 16 and greater than 20.

--- Okta.psm1
+++ Okta.psm1
@@ -2412,7 +2412,7 @@ function oktaDelUserFromRoles()
     (
         [parameter(Mandatory=$false)][ValidateLength(1,100)][String]$oOrg=$oktaDefOrg,
         [parameter(Mandatory=$true)][ValidateLength(20,20)][String]$uid,
-        [parameter(Mandatory=$true)][ValidateLength(16,20)][String]$rid
+        [parameter(Mandatory=$true)][ValidateLength(14,24)][String]$rid
     )
        
     [string]$resource = "/api/v1/users/" + $uid + "/roles/" + $rid
@@ -2439,7 +2439,7 @@ function oktaGetRoleTargetsByUserId()
     (
         [parameter(Mandatory=$false)][ValidateLength(1,100)][String]$oOrg=$oktaDefOrg,
         [parameter(Mandatory=$true)][alias("userId")][ValidateLength(20,20)][String]$uid,
-        [parameter(Mandatory=$true)][alias("roleId")][ValidateLength(16,20)][String]$rid
+        [parameter(Mandatory=$true)][alias("roleId")][ValidateLength(14,24)][String]$rid
     )
        
     [string]$resource = "/api/v1/users/" + $uid + "/roles/" + $rid + "/targets/groups"
@@ -2466,7 +2466,7 @@ function oktaAddRoleTargetsByUserId()
     (
         [parameter(Mandatory=$false)][ValidateLength(1,100)][String]$oOrg=$oktaDefOrg,
         [parameter(Mandatory=$true)][alias("userId")][ValidateLength(20,20)][String]$uid,
-        [parameter(Mandatory=$true)][alias("roleId")][ValidateLength(16,20)][String]$rid,
+        [parameter(Mandatory=$true)][alias("roleId")][ValidateLength(14,24)][String]$rid,
         [parameter(Mandatory=$true)][alias("groupId")][ValidateLength(20,20)][String]$gid
     )
        
@@ -2494,7 +2494,7 @@ function oktaDelRoleTargetsByUserId()
     (
         [parameter(Mandatory=$false)][ValidateLength(1,100)][String]$oOrg=$oktaDefOrg,
         [parameter(Mandatory=$true)][alias("userId")][ValidateLength(20,20)][String]$uid,
-        [parameter(Mandatory=$true)][alias("roleId")][ValidateLength(16,20)][String]$rid,
+        [parameter(Mandatory=$true)][alias("roleId")][ValidateLength(14,24)][String]$rid,
         [parameter(Mandatory=$true)][alias("groupId")][ValidateLength(20,20)][String]$gid
     )
        
